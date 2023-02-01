236216827e5cb57b94ce4c06a0a6eac3c7eee79f
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-02-01 20:30:37 +0530

    Modified main class and added lenovo class

---
+++ main.java
@@ -0,0 +1,22 @@
+class Employee{
+
+	float salary = 4000;
+
+}
+
+class Programmer extends Employee{
+
+	int bonus = 10000'
+
+}
+
+public class main{
+	public static void main(String args[]){
+
+		Programmer programmer = new Programmer();
+		System.out.println(programmer.salary);
+		System.out.println(programmer.bonus);
+
+		}
+
+	}
