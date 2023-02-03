62dd76b3b71cac139f2616e0f13c1f3492e98b15
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-02-03 21:32:30 +0530

    Created Prime numbers program

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,21 +1,41 @@
+import java.util.Scanner;
+
 public class PrimeNumbers {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
+
+
+	
 		int number;
 		int i;
-		for(number = 1; number <= 50; number++) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		number = sc.nextInt();
+
+		if(number <= 0)  {
+			throw new Exception("Invalid number");
+				}
+
+		System.out.println("Prime numbers between 1 and " + number + " are :");
+
+		for(i = 1; i <= number; i++) {
+
 			 int count = 0;
-			 for (i = 2; i <= number - 1; i++) {
-				 if (number%i == 0) {
+
+			 for (int j = 2; j <= i - 1; j++) {
+				 if (i%j == 0) {
 					count++;
 				 }
 				 
 			}
 			 if (count == 0) {
 					System.out.println(i);
-				}
-				else {
+			}
+
+			else {
 					count  = 0;
-				}
+			}
 		 }
 	}
 	
