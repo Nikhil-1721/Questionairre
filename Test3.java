233858758d66dfbf46bae576a1a937839429ee21
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-02-09 19:26:51 +0530

    Test 3

---
+++ Test3.java
@@ -0,0 +1,18 @@
+class Test3 {
+		public static void main(String args[]){
+			String s1 = "Nikhil ";
+			String s2 = "Kanishka ";
+			String s3 = "Yash ";
+			String s4 = "Shruti ";
+			String s5 = "Rehan ";
+			
+			String s6 = s1.concat(s2);
+			String s7 = s6.concat(s3);
+			String s8 = s7.concat(s4);
+			String s9 = s8.concat(s5);
+
+
+			System.out.println(s9);	
+		}
+	
+	}
