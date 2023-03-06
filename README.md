423b54ca4f541cf7a82ce8bb6c22e72d36942253
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-03-06 20:55:04 +0530

    Deleted all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Questionairre
-This GitHub repository is for all the classwork that was assigned for practice of basic Java questions.
--- Test2.java
+++
@@ -1,7 +0,0 @@
-class Test2{
-		public static void main(String args[]){
-			String s = "Sachin" + 40 + 40 + 50 + 30;
-			System.out.println(s);
-		}
-		
-	}
